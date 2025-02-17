52adbfeed804a961682238586435b940895fee5c
sugam003 <mcs24045@iiitl.ac.in>
2025-02-17 21:55:03 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,13 +1,5 @@
-import pandas as pd
-import numpy as np
-import pickle
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
-
 """
-Data columns 
+Data fields
 N - ratio of Nitrogen content in soil
 P - ratio of Phosphorous content in soil
 K - ratio of Potassium content in soil
@@ -15,12 +7,26 @@ temperature - temperature in degree Celsius
 humidity - relative humidity in %
 ph - ph value of the soil
 rainfall - rainfall in mm
+month - best month for sowing
 """
 
-df = pd.read_csv('Crop_recommendation.csv')
-print(df.shape)
-df.head()
+print('DATA INFO')
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+df = pd.read_csv('Crop_recommendation.csv')
+# Encode categorical variables
+encoder = LabelEncoder()
+encoder.fit_transform([
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"])
+df['month'] = encoder.transform(df['month'])
+
+print(df.shape)
 y = df['label'].values
 df_X = df.drop(['label'],axis=1)
 df_X.head()
@@ -37,4 +43,7 @@ classifier.fit(X_train, y_train)
 accuracy = classifier.score(X_test, y_test)
 print(f"Accuracy: {accuracy:.2f}")
 
-pickle.dump(classifier,open("model.pkl","wb"))
+
+# Save the model & encoders
+with open("model.pkl", "wb") as f:
+    pickle.dump({"model": classifier, "encoders": encoder}, f)
